536db49ae68b255bd4c26afbf510097554820142
Yibang Chen <ychen622@gmail.com>
2019-08-07 19:22:57 -0700

    authorization checker

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,15 @@ const jwt = require('jsonwebtoken');
 
 exports.loginRequired = function(req, res, next) {
   try {
-
+    // Bearer ...token
+    const token = req.headers.authorization.split(" ")[1];
+    jwt.verity(token, process.env.SECRET_KEY, function(err, decoded){
+      if (decoded) return next();
+      return next({
+        status: 401,
+        message: 'Please log in first'        
+      })
+    })
   } catch(e) {
     return next({
       status: 401,
@@ -14,7 +22,15 @@ exports.loginRequired = function(req, res, next) {
 
 exports.ensureCorrectUser = function(req, res, next) {
   try {
-
+    const token = req.headers.authorization.split(" ")[1];
+    jwt.verity(token, process.env.SECRET_KEY, function(err, decoded){
+      if (decoded && decoded.id === req.params.id)
+        return next();
+      return next({
+        status: 401,
+        message: 'Not authorized!'
+      })
+    }    
   } catch(e) {
     return next({
       status: 401,
